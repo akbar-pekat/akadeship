2ce87ff51cf33f344bf283faecd29e5c38370296
Muhammad Akbar Endy Wijaya <42760151+akbar-pekat@users.noreply.github.com>
2021-06-13 05:54:38 +0800

    Add files via upload

--- mobile/main.js
+++ mobile/main.js
@@ -4,16 +4,29 @@ $("#search-menu").click(function() {
     $(".search-section").css("top", "0px");
     $("nav").css("box-shadow", "none");
     $("#search-menu .iconify").css("color", "#02B888");
+    $(".container").css("margin-top", "125px");
     searchsect = 1;
   } else if (searchsect == 1) {
     $(".search-section").css("top", "-100px");
     $("nav").css("box-shadow", "0 7px 10px 0 rgba(0,0,0,.05");
     $("#search-menu .iconify").css("color", "#000");
+    $(".container").css("margin-top", "85px");
     searchsect = 0;
   }
 });
 
-$('.navbottom .wrapper .item').click(function(){
-    $('.navbottom .wrapper .item').removeClass("active");
-    $(this).addClass("active");
+$(document).ready(function() {
+  var _originalSize = $(window).width() + $(window).height()
+  $(window).resize(function() {
+    if ($(window).width() + $(window).height() != _originalSize) {
+      $(".navbottom").hide();
+    } else {
+      $(".navbottom").fadeIn();
+    }
+  });
+});
+
+$('.navbottom .wrapper .item').click(function() {
+  $('.navbottom .wrapper .item').removeClass("active");
+  $(this).addClass("active");
 });
