baf23ce16fe67660de406b33636256f31a5776d3
Muhammad Akbar Endy Wijaya <42760151+akbar-pekat@users.noreply.github.com>
2021-09-16 17:07:49 +0700

    Add files via upload

---
+++ mobile/script.js
@@ -0,0 +1,69 @@
+$("#lv_beasiswa").hide();
+$("#lv_event").hide();
+$("#lv_profil").hide();
+
+$('.navbottom .menu').click(function () {
+    $('.navbottom .menu').removeClass("active");
+    $(this).addClass("active");
+});
+
+$("#home").click(function () {
+    $("#home_ic").attr("src", "icons/home_bold.svg");
+    $("#beasiswa_ic").attr("src", "icons/teacher_linear.svg");
+    $("#event_ic").attr("src", "icons/calendar_linear.svg");
+    $("#profil_ic").attr("src", "icons/user_linear.svg");
+
+    $("#lv_home").fadeIn();
+    $("#lv_beasiswa").hide();
+    $("#lv_event").hide();
+    $("#lv_profil").hide();
+});
+
+$("#beasiswa").click(function () {
+    $("#home_ic").attr("src", "icons/home_linear.svg");
+    $("#beasiswa_ic").attr("src", "icons/teacher_bold.svg");
+    $("#event_ic").attr("src", "icons/calendar_linear.svg");
+    $("#profil_ic").attr("src", "icons/user_linear.svg");
+
+    $("#loading").show();
+
+    $("#lv_home").hide();
+    $("#lv_beasiswa").hide();
+    setTimeout(function () {
+        $("#lv_beasiswa").fadeIn();
+        $("#loading").hide();
+    }, 1500);
+    
+    $("#lv_event").hide();
+    $("#lv_profil").hide();
+});
+
+$("#event").click(function () {
+    $("#home_ic").attr("src", "icons/home_linear.svg");
+    $("#beasiswa_ic").attr("src", "icons/teacher_linear.svg");
+    $("#event_ic").attr("src", "icons/calendar_bold.svg");
+    $("#profil_ic").attr("src", "icons/user_linear.svg");
+
+    $("#loading").show();
+
+    $("#lv_home").hide();
+    $("#lv_beasiswa").hide();
+    $("#lv_event").hide();
+    setTimeout(function () {
+        $("#lv_event").fadeIn();
+        $("#loading").hide();
+    }, 1500);
+    $("#lv_profil").hide();
+});
+
+$("#profil").click(function () {
+    $("#home_ic").attr("src", "icons/home_linear.svg");
+    $("#beasiswa_ic").attr("src", "icons/teacher_linear.svg");
+    $("#event_ic").attr("src", "icons/calendar_linear.svg");
+    $("#profil_ic").attr("src", "icons/user_bold.svg");
+
+    $("#lv_home").hide();
+    $("#lv_beasiswa").hide();
+    $("#lv_event").hide();
+    $("#lv_profil").fadeIn();
+});
